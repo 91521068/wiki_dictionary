b30b6805945e34f3ebdaa76769786a6e8652b7db
mahdi <mehdi.bagvand@gmail.com>
2016-09-23 04:01:05 +0330

    Version 6.5 (stable)

---
+++ initData/api.py
@@ -0,0 +1,36 @@
+import json
+file = open("wikipedia_links.json","r")
+links = []
+c1s = []
+c2s = []
+c3s = []
+
+d = json.loads(file.read())
+for item in d['topics']:
+	c2 = item['name']
+	for j in item['subTopics']:
+		links.append(j['link'])
+		c3s.append(j['name'])
+		c2s.append(c2)
+		c1s.append("")
+
+for i in links:
+	print "'" + i + "',"
+
+print "\n"
+
+for i in c3s:
+	print "'" + i + "',"
+
+print "\n"
+
+for i in c2s:
+	print "'" + i + "',"
+
+print "\n"
+
+for i in c1s:
+	print "'" + i + "',"
+
+print "\n"
+file.close()
